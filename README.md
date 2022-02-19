b917e3e7f17f1e2cb1792b40e4369b8312d40ed9
923444 <Javier.SanzGarcia@cognizant.com>
2022-02-20 00:13:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Exercise to prepare Liferay Backend Certification
---
+++ gradebook-web/src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.gradebook.web.constants;
+
+/**
+ * @author pracker
+ */
+public class GradebookPortletKeys {
+
+	public static final String Gradebook =
+		"com_liferay_training_gradebook_web_GradebookPortlet";
+
+}
