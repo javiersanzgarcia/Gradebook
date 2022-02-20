3e6174f62694ea23e6ab0fdd675a6f966784b697
923444 <Javier.SanzGarcia@cognizant.com>
2022-02-20 01:22:05 +0100

    Add Localization Resources

--- gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
+++ gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -7,21 +7,19 @@ import org.osgi.service.component.annotations.Component;
 import javax.portlet.Portlet;
 
 /**
- * @author pracker
+ * @author Liferay
  */
 @Component(
         immediate = true,
         property = {
-                "com.liferay.portlet.header-portlet-css=/css/main.css",
-                "javax.portlet.display-name=Gradebook",
-                "javax.portlet.init-param.template-path=/",
-                "javax.portlet.init-param.view-template=/view.jsp",
-                "javax.portlet.name=" + GradebookPortletKeys.Gradebook,
-                "javax.portlet.resource-bundle=content.Language",
-                "javax.portlet.security-role-ref=power-user,user",
                 "com.liferay.portlet.display-category=category.training",
                 "com.liferay.portlet.instanceable=false",
-                "javax.portlet.init-param.add-process-action-success-action=false"
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/view.jsp",
+                "javax.portlet.init-param.add-process-action-success-action=false",
+                "javax.portlet.name=" + GradebookPortletKeys.Gradebook,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=power-user,user"
         },
         service = Portlet.class
 )
